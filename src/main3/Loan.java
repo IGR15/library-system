91bdc77a628ac4bdd6ab4fcc9162fdde5560e9c5
IGR15 <151957251+IGR15@users.noreply.github.com>
2023-12-25 15:17:03 +0200

    Update Loan.java
    updated

--- src/main3/Loan.java
+++ src/main3/Loan.java
@@ -2,26 +2,70 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main3;
+package libraryapp;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- *
- * @author MSI
- */
+
 public class Loan {
-    private int id;
+    final int loannum;
     private Book book;
     private Student student;
     private Date dueDate;
+    private static int count = 0;
 
-    public Loan(int id, Book book, Student student, Date dueDate) {
-        this.id = id;
+    public Loan(Book book, Student student, Date dueDate) {
+        this.loannum = count++;
         this.book = book;
         this.student = student;
         this.dueDate = dueDate;
     }
     
-   
+    public int getLoannum() {
+        return loannum;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getInfo() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("M/d/yyyy");
+        String formattedDate = dateFormat.format(dueDate);
+        return "loan number "+ loannum + "\nBook: " + book.getTitle() + "\nStudent: " + student.getName() + "\nID:"+student.getId()+ "\nDue Date: " + formattedDate;
+    }
+    
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("M/d/yyyy");
+        String formattedDate = dateFormat.format(dueDate);
+        
+        return book.getNo() + "," + student.getId() + "," + formattedDate;
+    }
+    
+    public boolean isOverdue() {
+        Date currentDate = new Date();
+        return currentDate.after(dueDate);
+    }
 }
