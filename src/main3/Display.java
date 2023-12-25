de18c9519600584f620bf128f1843e87ab0dfc9c
IGR15 <151957251+IGR15@users.noreply.github.com>
2023-12-25 15:14:09 +0200

    Update Display.java
    updated

--- src/main3/Display.java
+++ src/main3/Display.java
@@ -2,24 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main3;
-
-/**
- *
- * @author MSI
- */
 public interface Display {
-    
-    /**
-     *
-     * @return
-     */
     public String getinfo();
-
-    /**
-     *
-     * @return
-     */
     public boolean inLoan();
-  
 }
